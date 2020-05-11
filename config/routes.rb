5e17190f4f04c4534997e5d0bd37c2cd660ba749
Douglas Fraga <douglas.fraga@gmail.com>
2020-05-11 18:54:48 -0300

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'prtfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  get 'about', to: 'pages#about'
+  get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
   resources :blogs
